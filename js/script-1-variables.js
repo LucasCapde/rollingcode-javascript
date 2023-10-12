fb058e80f09a8fbb1bbf1d13a73f10d5d07ecfca
Lucas Capdevila <lcapdevila60@gmail.com>
2023-10-12 18:32:37 -0300

    se agrega info sobre variables

---
+++ js/script-1-variables.js
@@ -0,0 +1,34 @@
+// Esto es un comentario de una línea
+
+/* 
+Esto es un comentario
+múltiples 
+líneas.
+*/
+
+
+//  Declarando una variable (let / var (no recomendada) / const)
+let anioActual
+
+//  Inicializando una variable; le doy un valor, por ej Númerico
+anioActual = 2023
+
+//  Además, pueden haber valores de tipo String
+let nombre = 'Lucas'
+
+//  De tipo booleano (Verdadero o falso)
+let mayorDeEdad = true
+let desempleado = false
+
+//  De tipo array (listas)
+let animales = ['perro', 'gato']
+
+//  De tipo objeto
+let auto = {
+  marca: 'Ford',
+  nombre: 'Mustang',
+  modelo: '1978'
+}
+
+//  Tipo vacío (null)
+let empty = null
