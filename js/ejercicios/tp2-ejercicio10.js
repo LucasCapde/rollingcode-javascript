fb394c2c8e0a778d7b8a010cc84d26bdde00a6ab
Lucas Capdevila <lcapdevila60@gmail.com>
2023-10-26 18:33:43 -0300

    se dibujan los numeros en orden descendente

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -7,13 +7,13 @@ Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 const filas = parseInt(prompt('Ingrese un número de filas'))
 const columnas = parseInt(prompt('Ingrese un número de columnas'))
 
-const totalCeldas = filas * columnas
+let totalCeldas = filas * columnas
 
 document.write(`<table><tbody>`)
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
   document.write(`<tr>`)
   for (let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++) {
-    document.write(`<td>33</td>`)    
+    document.write(`<td>${totalCeldas--}</td>`)    
   }
   document.write(`</tr>`)
 }
