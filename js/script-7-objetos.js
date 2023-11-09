3dbbbd63a9e70d03ac166ac1be7d5314b4636659
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-09 18:35:01 -0300

    introducción a objetos

---
+++ js/script-7-objetos.js
@@ -0,0 +1,44 @@
+/*
+Un objeto es un valor nativo de JavaScript. Representa una porción de la realidad.
+Para declararlo usamos la notación literal:
+*/
+
+//* Notación literal:
+const pelicula = {
+  //  Propiedades: Están compuestas de (clave: valor)
+  titulo: 'La máscara',
+  anio: 1994,
+  duracion: '1h 40 min',
+  genero: ['comedia', 'fantasía'],
+
+  //  Métodos
+  iniciarPeli: function (){
+    document.write(`<p>Comenzó la película...</p>`)
+  },
+  pausarPeli: () => {
+    document.write(`<p>Se pausó la película...</p>`)
+  }
+}
+
+//* Mostrar el objeto:
+console.log(pelicula);
+document.write(pelicula)
+document.write(`<h1>Película: ${pelicula.titulo}</h1>`)
+document.write(`<p>Duración: ${pelicula.duracion}</p>`)
+document.write(`<p>Año de estreno: ${pelicula['anio']}</p>`)
+//  Cuando me equivoco en el nombre de la propiedad
+document.write(`<p>Duración: ${pelicula.duraciom}</p>`)
+
+
+
+//* Agregarle nuevas propiedades a un objeto:
+pelicula.estado = true;
+console.log(pelicula);
+pelicula.genero.push('aventura')
+console.log(pelicula.genero);
+
+
+
+//* Modificar propiedades a un objeto
+pelicula.estado = false;
+console.log(pelicula);
